3b7ae3bc86a26ac0d56199a090a12c9439b05974
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-06-03 16:03:52 +0900

    [Silver IV] Title: ATM, Time: 272 ms, Memory: 20312 KB -BaekjoonHub

--- Java/백준/Silver/11399. ATM/ATM.java
+++ Java/백준/Silver/11399. ATM/ATM.java
@@ -18,11 +18,12 @@ public class Main
 		    
 		Arrays.sort(time);
 		int sum = 0;
+		int allSum = 0;
 		
 		for(int i = 0; i < n; i++)
 		{
-		    for(int j = 0; j <= i; j++)
-		        sum += time[j];
+		    allSum += time[i];
+            sum += allSum;
 		}
 		
 		System.out.println(sum);
