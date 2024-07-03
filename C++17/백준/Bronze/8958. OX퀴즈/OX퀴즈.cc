26195182ef187cbb8977276351860c890841f9fb
hyorim jo <133748490+hyorim-jo@users.noreply.github.com>
2024-07-03 13:54:08 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    string data;
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++)
+    {
+        int ans = 0, scr = 0;
+        
+        cin >> data;
+        for(int j = 0; data[j] == 'O' || data[j] == 'X'; j++)
+        {
+            if(data[j] == 'O')
+            {
+                scr++;
+                ans += scr;
+            }
+            else scr = 0;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
