bf093f2c3b12de086471981cce883ea867531125
hyorim jo <20231164@sungshin.ac.kr>
2026-03-08 10:48:21 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a{}, b{};
+
+    // 첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
+    cin >> a >> b;
+
+    // 첫째 줄에 A+B를 출력한다.
+    cout << a + b;
+    
+    return 0;
+}
